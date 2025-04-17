ae0f298804a822cf203e4223f4583907e29e1572
Elven Zheng <elvekins@gmail.com>
2025-04-17 16:09:09 -0400

    Initial commit

---
+++ app/components/nav.tsx
@@ -0,0 +1,10 @@
+import Link from "next/link";
+
+export default function Nav() {
+    const linkStyle = "p-1 m-2 text-xl hover:underline hover:bg-violet-300"
+    return (
+        <nav className="p-2 m-4">
+            <Link href={`/`} className={linkStyle}>Home</Link>
+        </nav>
+    )
+}
