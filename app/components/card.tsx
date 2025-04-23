3a4b2de0bec3ab08cd8b504d4a6c215402bc6dec
Elven Zheng <elvekins@gmail.com>
2025-04-23 13:16:14 -0400

    looks like issue has been fixed.

--- app/components/card.tsx
+++ app/components/card.tsx
@@ -5,7 +5,7 @@ import Image from "next/image";
 export default function Card({ song }: { song: PropTypes }) {
     return (
         <>
-            <Link href={`/${song.id}`} className="block w-full ">
+            <Link href={`/${song.id}`} className="block w-full bg-amber-200 px-7 py-1 my-3">
                 <div className="flex flex-row items-center justify-between">
                     <Image
                         src={song.img}
@@ -14,7 +14,7 @@ export default function Card({ song }: { song: PropTypes }) {
                         height={64}
                         className="object-cover"
                     />
-                    <h4 className="font-bold text-xl mx-3">{song.title}</h4>
+                    <h4 className="font-bold text-xl mx-5">{song.title}</h4>
                 </div>
             </Link>
         </>
