9b509be724b3465e393e138bab19a59f91106993
Elven Zheng <elvekins@gmail.com>
2025-04-24 11:56:09 -0400

    placed comment inside braces

--- app/components/homepage.tsx
+++ app/components/homepage.tsx
@@ -7,7 +7,7 @@ export default function Homepage() {
         <div className="flex justify-center w-full">
             <div className="block m-4">
 
-                /* replace this with a button to redirect to a random page in the playlist*/
+                {/* replace this with a button to redirect to a random page in the playlist*/}
                 <div className="bg-red-500 text-green-300">
                     <p>play random song button</p>
                 </div>
