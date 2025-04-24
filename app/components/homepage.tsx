f27cc2880e323795c7d1b98028e0d89ded4c6cec
Elven Zheng <elvekins@gmail.com>
2025-04-24 11:51:03 -0400

    Made playlist its own component, added some comments

--- app/components/homepage.tsx
+++ app/components/homepage.tsx
@@ -1,28 +1,18 @@
 'use client';
-import Card from "@/app/components/card";
-import songsData from "@/songs.json";
-import { useTheme } from "@/lib/useTheme"
+
+import Playlist from "@/app/components/playlist";
 
 export default function Homepage() {
-    const {currentTheme} = useTheme();
     return (
         <div className="flex justify-center w-full">
             <div className="block m-4">
+
+                /* replace this with a button to redirect to a random page in the playlist*/
                 <div className="bg-red-500 text-green-300">
                     <p>play random song button</p>
                 </div>
-              
-                <div
-                    className="flex justify-center px-13 py-3 m-8 rounded-xl"
-                    style={{backgroundColor: currentTheme.secondary}}
-                >
-                    <div className="block h-150 w-full overflow-y-auto p-4">
-                        {songsData.songs.map((song) => (
-                            <Card key={song.id} song={song} />
-                        ))}
-                    </div>
-                </div>
 
+                <Playlist/>
             </div>
         </div>
     );
---
+++ app/components/playlist.tsx
@@ -0,0 +1,22 @@
+import songsData from "@/songs.json";
+import Card from "@/app/components/card";
+import { useTheme } from "@/lib/useTheme"
+
+export default function Playlist() {
+    const {currentTheme} = useTheme();
+    return(
+        <>
+            <div
+                className="flex justify-center px-13 py-3 m-8 rounded-xl"
+                style={{backgroundColor: currentTheme.secondary}}
+            >
+                <div className="block h-150 w-full overflow-y-auto p-4">
+                    {songsData.songs.map((song) => (
+                        <Card key={song.id} song={song} />
+                    ))}
+                </div>
+            </div>
+        </>
+    );
+
+}
