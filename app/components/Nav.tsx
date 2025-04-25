7dd68f66f70d807cf93a57003ea5aa6f193e4ea7
Elven Zheng <elvekins@gmail.com>
2025-04-25 12:36:04 -0400

    made hover scale instead of change bg color

--- app/components/Nav.tsx
+++ app/components/Nav.tsx
@@ -1,7 +1,8 @@
+// basic Nav function.
 import Link from "next/link";
 
 export default function Nav() {
-    const linkStyle = "p-1 font-bold text-lg hover:underline hover:bg-gray-400";
+    const linkStyle = "p-1 font-bold text-lg hover:scale-[1.03]";
 
     return (
         <nav className="p-2">
