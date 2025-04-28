3ff4fe81bce786d80ccf965e5a70fe1f406e3b8b
codyheadings <codyheadings05@gmail.com>
2025-04-27 22:56:48 -0400

    improved DRY on footer and completed randomize button

--- lib/randomize.ts
+++ lib/randomize.ts
@@ -1,9 +1,24 @@
 //Cody
 
+import songsData from "@/songs.json";
+import {redirect} from "next/navigation";
+
 export default function randomize(){
-    //pseudocode for later:
     /*
-        generate random element from list of all titles in songData
-        redirect user to /[title]
+        generate random element from list of all titles in songsData
+        redirect user to /[songsData.title]
      */
+    // find max index based on length so we don't have to adjust this function if new songs are added
+    const maxNum = songsData.songs.length;
+    const random = Math.floor(Math.random() * (maxNum));
+    // get element of songsData songs object based on scaled random number generator
+    const song = songsData.songs[random];
+
+    if (!song) {
+        console.error("Song not found!");
+        return null;
+    }
+
+    console.log(`Redirecting to song ${song.title}`);
+    return redirect(`/${song.title}`);
 }
